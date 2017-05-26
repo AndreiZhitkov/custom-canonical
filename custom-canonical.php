84a86e2893ba5f4b9c1cf58e6b201c62853e2f70
Andrei Zhitkov <zhitkov.andrei@gmail.com>
2017-05-26 00:49:05 -0500

    Initial release

---
+++ custom-canonical.php
@@ -0,0 +1,107 @@
+<?php
+/*
+Plugin Name: Custom canonical URL for Yoast SEO
+Plugin URI:  https://github.com/AndreiZhitkov/custom-canonical
+Description: Set default custom canonical URL for Yoast SEO. The setting is found in Settings -> General.
+Version:     1.0
+Author:      Andrei Zhitkov
+Author URI:  https://github.com/AndreiZhitkov
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: customcanonical
+Domain Path: /languages
+*/
+
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+function customcanonical_discover_yoast() {
+	$customcanonical_yoast = FALSE;
+		if ( is_plugin_active( 'wordpress-seo/wp-seo.php' ) ) {
+			$customcanonical_yoast = TRUE;
+		}
+	return $customcanonical_yoast;
+}
+
+function admin_notice_noyoast() { ?>
+    <div class="notice notice-error">
+    <p><?php _e( '<strong>Custom canonical URL:</strong> Yoast SEO is not active', 'customcanonical' ); ?></p>
+    </div>
+<?php }
+
+function customcanonical_init () {
+	if ( customcanonical_discover_yoast() == FALSE ) {
+		add_action( 'admin_notices', 'admin_notice_noyoast');
+		deactivate_plugins( plugin_basename( __FILE__ ) );
+	} else {
+		add_settings_section(
+		'customcanonical_setting_section',
+		'Default canonical URL for Yoast SEO',
+		'customcanonical_section_callback',
+		'general'
+		);
+
+		add_settings_field(
+		'customcanonical_url',
+		'Canonical URL',
+		'customcanonical_setting_callback',
+		'general',
+		'customcanonical_setting_section'
+		);
+
+		register_setting(
+		'general',
+		'customcanonical_url'
+		);
+	}
+}
+add_action( 'admin_init', 'customcanonical_init' );
+
+/* load text domain for this plugin | it will not load properly without add_action() */
+function load_customcanonical_textdomain () {
+		load_plugin_textdomain('customcanonical', false, basename( dirname( __FILE__ ) ) . '/languages/' );
+}
+add_action( 'plugins_loaded', 'load_customcanonical_textdomain' );
+
+/* all is good to go */
+/* adminirtative interface */
+ function customcanonical_section_callback () {
+ 	echo '<p>'. _e( 'Enter your canonical URL. Do not include the transfer protocol or trailing slash.', 'customcanonical' ) .'</p>';
+}
+
+function customcanonical_setting_callback () {
+ 	//echo '<form method="post" action="options.php">';
+	//settings_fields( 'permalink' );
+ 	echo "<input name='customcanonical_url' id='customcanonical_url' type='text' value='". get_option('customcanonical_url') ."' class='regular-text code' placeholder='yourcanonical.url' /><code>/your-pretty-permalink</code>";
+}
+
+apply_filters( 'sanitize_option_customcanonical_url', 'customcanonical_url', 'customcanonical_url' );
+
+/*
+* Plugin Name: WPSE WPSEO Canonical
+* Plugin URI: http://wordpress.stackexchange.com
+* Description: Changes canonical url domain.
+* Author: Sisir
+* Version: 1.0
+* Author URI: http://developerpage.net
+**/
+
+$domain = get_option('customcanonical_url', '');
+function customcanonical_domain_replace ($url){
+	global $domain;
+	$parsed = parse_url(home_url());
+	$current_site_domain = $parsed['host'];
+	return str_replace($current_site_domain, $domain, $url);
+}
+
+function customcanonical_domain_set () {
+	global $domain;
+		if ( empty( $domain )) {
+			return;
+		} else {
+			add_filter('wpseo_canonical', 'customcanonical_domain_replace');
+			add_filter('wpseo_prev_rel_link', 'customcanonical_domain_replace');
+			add_filter('wpseo_next_rel_link', 'customcanonical_domain_replace');
+		}
+}
+add_action( 'plugins_loaded', 'customcanonical_domain_set' );
+?>
